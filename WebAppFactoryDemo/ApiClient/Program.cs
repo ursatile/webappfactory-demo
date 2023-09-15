0cfee372930af27545845b0ac5e0f3ec96cce11d
Dylan Beattoie <dylan@dylanbeattie.net>
2023-09-15 10:18:10 +0200

    Very simple client + server

---
+++ WebAppFactoryDemo/ApiClient/Program.cs
@@ -0,0 +1,7 @@
+﻿using RestSharp;
+
+var options = new RestClientOptions("http://localhost:5279");
+var client = new RestClient(options);
+var request = new RestRequest("/weatherforecast");
+var response = await client.GetAsync(request);
+Console.WriteLine(response.Content);
